12f6a615be5d1757bf2e0512507853dad568b6dd
iri.sobakina <iri.dolgopolova@gmail.com>
2023-01-02 16:15:25 +0300

    Добавляет решение третьей практической работы

---
+++ homework3.js
@@ -0,0 +1,30 @@
+const firstIpForSearch = '89.123.1.41';
+const secondIpForSearch = '34.48.240.111';
+
+const fs = require('fs');
+const readStream = fs.createReadStream('./access_tmp.log', 'utf-8');
+const firstWriteStream = fs.createWriteStream(`${firstIpForSearch}`);
+const secondWriteStream = fs.createWriteStream(`${secondIpForSearch}`);
+
+let lineNumber = 0;
+
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: readStream
+});
+
+rl.on('line', (line) => {
+    if (line.includes(firstIpForSearch)) {
+        firstWriteStream.write(line + '\n');
+        console.log(`Найдена записть про IP ${firstIpForSearch} на строке ${lineNumber}`);
+
+    }
+
+    if (line.includes(secondIpForSearch)) {
+        secondWriteStream.write(line + '\n');
+        console.log(`Найдена записть про IP ${secondIpForSearch} на строке ${lineNumber}`);
+
+    }
+
+    ++lineNumber;
+});
